1ff0e6ae866f8ba16c0e8b6d8a7cab231cc13de7
TH8359 <136388415+TH8359@users.noreply.github.com>
2023-06-12 21:36:03 -0400

    Add files via upload

---
+++ TA5/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Enter file name");
+var fileName = Console.ReadLine();
+if (fileName == "person1")
+{
+    Console.WriteLine($"{Environment.NewLine}{fileName} located. Please enter the following information");
+    Console.WriteLine("First Name:");
+    var firstName = Console.ReadLine();
+    Console.WriteLine("Last Name:");
+    var lastName = Console.ReadLine();
+    Console.WriteLine("Email Address:");
+    var emailAddress = Console.ReadLine();
+    if ((firstName == "Tyler") & (lastName == "Hogue") & (emailAddress == "testemail.com"))
+    {
+        Console.WriteLine($"{Environment.NewLine}{firstName} {lastName} @{emailAddress}");
+        Console.WriteLine($"{Environment.NewLine}{fileName} has been verified. Press any key to exit");
+        Console.ReadKey(true);
+    }
+    else
+    {
+        Console.WriteLine($"{Environment.NewLine}{fileName} information entered is incorrect. To try again, press any key to exit");
+        Console.ReadKey(true);
+    }
+}
+else
+{
+    Console.WriteLine($"{Environment.NewLine}{fileName} could not be found. To try again, press any key to exit");
+    Console.ReadKey(true);
+}
+
